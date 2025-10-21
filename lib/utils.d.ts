2cce05ba1bd2ecbb41dd49e3ef97a52bb2c01d1f
Foulks-Plb <bananox88@gmail.com>
2025-10-21 14:13:29 +0200

    Enhance development setup and documentation

--- lib/utils.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const function1: () => void;
--- test/test1.test.ts
+++ test/test1.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect } from "vitest";
+import { describe } from "vitest";
 import { test } from "./setup";
 import { function1 } from "src/utils";
 
