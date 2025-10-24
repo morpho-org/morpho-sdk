44dd380918694c55e5cc27ef3862f61fd0ddb0cc
Foulks-Plb <bananox88@gmail.com>
2025-10-24 19:22:05 +0200

    feat(tests): update redeem and withdraw tests for VaultV2 functionality

--- test/actions/redeem.test.ts
+++ test/actions/redeem.test.ts
@@ -1,16 +1,13 @@
 import { describe, expect } from "vitest";
 import { test } from "../setup";
 
-import {
-  instantiateVaultV2,
-  createMorphoClient,
-  withdrawVaultV2,
-  redeemVaultV2,
-} from "src";
+import { instantiateVaultV2, createMorphoClient, redeemVaultV2 } from "src";
 import { KeyrockUsdcVaultV2 } from "test/fixtures/vaultV2";
+import { parseUnits } from "viem";
+import { testInvariants } from "test/helpers/invariants";
 
-describe("Deposit VaultV2", () => {
-  test("should create deposit bundle", async ({ client }) => {
+describe("Redeem VaultV2", () => {
+  test("should create redeem transaction", async ({ client }) => {
     const morpho = createMorphoClient(client);
 
     const redeem = (await morpho.vaultV2(KeyrockUsdcVaultV2.address)).redeem({
@@ -36,5 +33,45 @@ describe("Deposit VaultV2", () => {
     expect(redeem).toBeDefined();
     expect(redeem.tx).toStrictEqual(redeem_2.tx);
     expect(redeem_3).toStrictEqual(redeem_2.tx);
+  });
+
+  test("should redeem 1K shares in vaultV2", async ({ client }) => {
+    const shares = parseUnits("1000", 18);
+    const assets = parseUnits("1000", 6);
+    await client.deal({
+      erc20: KeyrockUsdcVaultV2.address,
+      amount: shares,
+    });
+
+    const {
+      vaults: {
+        KeyrockUsdcVaultV2: { initialState, finalState },
+      },
+    } = await testInvariants({
+      client,
+      params: {
+        vaults: { KeyrockUsdcVaultV2 },
+      },
+      actionFn: async () => {
+        const morpho = createMorphoClient(client);
+        const vaultV2 = await morpho.vaultV2(KeyrockUsdcVaultV2.address);
+        const withdraw = vaultV2.withdraw({
+          assets,
+        });
+
+        await morpho.walletClient.sendTransaction(withdraw.tx);
+      },
+    });
+
+    expect(finalState.userSharesBalance).toBeLessThan(
+      initialState.userSharesBalance
+    );
+    expect(finalState.userAssetBalance).toEqual(
+      initialState.userAssetBalance + assets
+    );
+    expect(finalState.userSharesBalance).toEqual(1004842842n);
+    expect(finalState.morphoAssetBalance).toEqual(
+      initialState.morphoAssetBalance - assets
+    );
   });
 });
