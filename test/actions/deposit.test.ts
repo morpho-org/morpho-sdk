30b156e884d0bf0a5f36cd09fd5fc876a95dc5f6
Foulks-Plb <bananox88@gmail.com>
2025-11-26 19:45:38 +0100

    test(deposit): uncomment deposit test

--- test/actions/deposit.test.ts
+++ test/actions/deposit.test.ts
@@ -3,6 +3,8 @@ import { KeyrockUsdcVaultV2 } from "test/fixtures/vaultV2";
 import { mainnet } from "viem/chains";
 import { describe, expect } from "vitest";
 import { test } from "../setup";
+import { testInvariants } from "test/helpers/invariants";
+import { parseUnits } from "viem";
 
 describe("Deposit VaultV2", () => {
   test("should create deposit bundle", async ({ client }) => {
@@ -47,51 +49,52 @@ describe("Deposit VaultV2", () => {
     expect(data.address).toStrictEqual(KeyrockUsdcVaultV2.address);
   });
 
-  // test("should deposit 1K USDC in vaultV2", async ({ client }) => {
-  //   const amount = parseUnits("1000", 6);
-  //   await client.deal({
-  //     erc20: KeyrockUsdcVaultV2.asset,
-  //     amount: amount,
-  //   });
+  test("should deposit 1K USDC in vaultV2", async ({ client }) => {
+    const amount = parseUnits("1000", 6);
+    await client.deal({
+      erc20: KeyrockUsdcVaultV2.asset,
+      amount: amount,
+    });
 
-  //   const {
-  //     vaults: {
-  //       KeyrockUsdcVaultV2: { initialState, finalState },
-  //     },
-  //   } = await testInvariants({
-  //     client,
-  //     params: {
-  //       vaults: { KeyrockUsdcVaultV2 },
-  //     },
-  //     actionFn: async () => {
-  //       const morpho = createMorphoClient(client);
-  //       const vaultV2 = morpho.vaultV2(KeyrockUsdcVaultV2.address);
-  //       const deposit = await vaultV2.deposit({
-  //         userAddress: client.account.address,
-  //         assets: amount,
-  //       });
+    const {
+      vaults: {
+        KeyrockUsdcVaultV2: { initialState, finalState },
+      },
+    } = await testInvariants({
+      client,
+      params: {
+        vaults: { KeyrockUsdcVaultV2 },
+      },
+      actionFn: async () => {
+        const morpho = createMorphoClient(client);
+        const vaultV2 = morpho.vaultV2(KeyrockUsdcVaultV2.address);
+        const deposit = await vaultV2.deposit({
+          userAddress: client.account.address,
+          assets: amount,
+        });
 
-  //       const requirements = await deposit.getRequirements();
+        const tx = deposit.build();
+        const requirements = await deposit.getRequirements();
 
-  //       const approveTx = requirements[0];
-  //       if (!approveTx) {
-  //         throw new Error("Approve transaction not found");
-  //       }
+        const approveTx = requirements[0];
+        if (!approveTx) {
+          throw new Error("Approve transaction not found");
+        }
 
-  //       await client.sendTransaction(approveTx);
-  //       await client.sendTransaction(deposit.tx);
-  //     },
-  //   });
+        await client.sendTransaction(approveTx);
+        await client.sendTransaction(tx);
+      },
+    });
 
-  //   expect(finalState.userAssetBalance).toEqual(
-  //     initialState.userAssetBalance - amount
-  //   );
-  //   expect(finalState.morphoAssetBalance).toEqual(
-  //     initialState.morphoAssetBalance + amount
-  //   );
-  //   expect(finalState.userSharesBalance).toBeGreaterThan(
-  //     initialState.userSharesBalance
-  //   );
-  //   expect(finalState.userSharesBalance).toEqual(995180492265720444556n);
-  // });
+    expect(finalState.userAssetBalance).toEqual(
+      initialState.userAssetBalance - amount
+    );
+    expect(finalState.morphoAssetBalance).toEqual(
+      initialState.morphoAssetBalance + amount
+    );
+    expect(finalState.userSharesBalance).toBeGreaterThan(
+      initialState.userSharesBalance
+    );
+    expect(finalState.userSharesBalance).toEqual(995180492265720444556n);
+  });
 });
