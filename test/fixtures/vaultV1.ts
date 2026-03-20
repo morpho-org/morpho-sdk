2efedef7c226b70737548880f1ed5f9213b07a0c
foulk <bananox88@gmail.com>
2026-03-20 17:22:47 +0100

    feat(vaultV1): add unit tests for deposit, redeem, and withdraw actions

---
+++ test/fixtures/vaultV1.ts
@@ -0,0 +1,9 @@
+export const SteakhouseUsdcVaultV1 = {
+  address: "0xbEef01735c132AdA46Aa9aa9b578D86382D5F212",
+  asset: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+} as const;
+
+export const GauntletWethVaultV1 = {
+  address: "0x4881Ef0BF6d2365D3dd6499ccd7532bcdBCE0658",
+  asset: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+} as const;
